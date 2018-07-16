e170f3e595404b7cea27e4c3cbafe3668df25fd3
Tiana Nguyen <tiananguyen99@gmail.com>
2018-07-15 19:54:16 -0700

    Pinterest Project edit

--- pinterest/pinterest.js
+++ pinterest/pinterest.js
@@ -3,7 +3,7 @@ PDK.init({ appId: '4976224077197881480', cookie: true });
 
 // Pinterest log in
 function login() {
-  PDK.login(function(response) {
+  PDK.login({ scope : 'write_public, read_public' }, function(response) {
     console.log(response); //TEST to see response status
     // Display the status of their login
     if (response.status === 'connected') {
@@ -49,7 +49,7 @@ var params = {
 // Retrieve pins on the board
 var pins = [];
 function getPins(data, callback) {
-  PDK.request('/boards/Techies/pins', 'GET', function (response) { // TEMP BOARD : Techies
+  PDK.request('/boards/Cooking Sessions/pins', 'GET', function (response) { // TEMP BOARD : Techies
     if (!response || response.error) {
       alert('Error occurred');
     } else {
@@ -80,3 +80,49 @@ function findDupPins(data) {
     }
   }
 }
+
+function pinterest() {
+  // Initialize Pinterest JavaScript SDK
+  window.pAsyncInit = function() {
+      PDK.init({
+          appId: "4976224077197881480",
+          cookie: true
+      });
+  };
+
+  (function(d, s, id){
+      var js, pjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//assets.pinterest.com/sdk/sdk.js";
+      pjs.parentNode.insertBefore(js, pjs);
+  }(document, 'script', 'pinterest-jssdk'));
+
+  // Function to log into user's Pinterest
+    PDK.login({ scope : 'write_public, read_public' }, function(response) {
+      console.log(response); //TEST TO SEE STATUS
+      // Display the status of their login
+      if (response.error || !response) {
+        document.getElementById('show').innerHTML = 'You are not connected. Please try again.';
+      } else {
+        document.getElementById('show').innerHTML = 'You are connected! We will now be deleting duplicate pins.';
+        // Get board information
+        var pins = [];
+        var board_id='Cooking Sessions'; // TEMP BOARD
+        PDK.request('/boards/'+board_id+'/pins/', function (response) {
+            if (!response || response.error) {
+                alert('Error occurred');
+            } else {
+                pins = pins.concat(response.data);
+                if (response.hasNext) {
+                response.next();
+                }
+            }
+        });
+        document.getElementById('show').innerHTML = pins;
+        // Look for duplicate pins & delete
+        // getPins(data, response);
+        // findDupPins(data);
+      }
+    });
+  }
