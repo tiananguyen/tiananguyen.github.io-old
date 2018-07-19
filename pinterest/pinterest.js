5d2590d213120825257507c0e92ac9b8a51682db
Tiana Nguyen <tiananguyen99@gmail.com>
2018-07-19 15:38:23 -0700

    Delete test

--- pinterest/pinterest.js
+++ pinterest/pinterest.js
@@ -60,7 +60,7 @@ function pinterest() {
           } else {
             pins = pins.concat(response.data);
             document.getElementById('show').innerHTML = response.data; // Display pins
-            deletePin(pins,callback); // TEST DELETING PINS
+            deletePin(pins,function(response){}); // TEST DELETING PINS
             if (response.hasNext) {
               response.next();
             }
