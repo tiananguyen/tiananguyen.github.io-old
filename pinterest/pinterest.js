a5d27b02b5536a11e7a11c1f1699e3c762f18848
Tiana Nguyen <tiananguyen99@gmail.com>
2018-07-19 17:13:37 -0700

    Test

--- pinterest/pinterest.js
+++ pinterest/pinterest.js
@@ -54,7 +54,7 @@ function pinterest() {
       console.log("Test 3"); // TEST
       PDK.request('/boards/'+ user_id +'/'+ board_id +'/pins/', { fields: 'note,image[small]' }, function (response) {  // Get board information
           console.log(response); // TEST
-          console.log(data.pins[0].note); // TEST
+          console.log(response.pins[0].note); // TEST
           console.log("Test 4"); // TEST
           if (!response || response.error) {
             alert('Error occurred');
