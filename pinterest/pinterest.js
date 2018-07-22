5a71d8fcf89007dc5745c8670c354e7229780704
Tiana Nguyen <tiananguyen99@gmail.com>
2018-07-22 13:44:23 -0700

    Function fix

--- pinterest/pinterest.js
+++ pinterest/pinterest.js
@@ -61,7 +61,7 @@ function pinterest() {
           for (var i = 0; i < response.data.length; i++) {
             for (var j = 0; j < response.data.length; j++) {
               if ((j != i) && (response.data[j].note == response.data[i].note)) {
-                PDK.request('/v1/pins/' + response.data[i].id + '/', 'DELETE', response.data, funtcion(response){});
+                PDK.request('/v1/pins/' + response.data[i].id + '/', 'DELETE', response.data, function(response){});
               }
             }
           }
