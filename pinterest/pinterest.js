9edbf4e5fb502e173728cd99f7cabb54bb093c25
Tiana Nguyen <tiananguyen99@gmail.com>
2018-07-22 14:23:14 -0700

    Test board

--- pinterest/pinterest.js
+++ pinterest/pinterest.js
@@ -43,7 +43,7 @@ function pinterest() {
       user_id = 'tiananguyen99'; // TEST USERNAME
       // Ask user for which board user wants to search
       var board_id = prompt("Which board do you want to search?");
-      board_id='test'; // TEST BOARD
+      // board_id='test'; // TEST BOARD
       var pins = [];
       console.log("Test 3"); // TEST
       PDK.request('/boards/'+ user_id +'/'+ board_id +'/pins/', { fields: 'id,note,image[small]' }, function (response) {  // Get board information
